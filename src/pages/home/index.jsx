0543fdf9da7e9d544e0e599d551d05a1a555b955
Salustiano007 <ra56242444879@acaonsfatima.org.br>
2024-11-08 00:05:17 -0300

    bug na responsividade

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -5,16 +5,16 @@ import './index.scss';
 export default function home(){
 
     const Div1 = () => <div className='barra'> 
-    <a  className='link' href='#'>Sobre Nós</a>
-    <a  className='link' href='#'>Orçamento</a>
-    <a  className='link' href='#'>Contato</a>
-    <a  className='link' href='#'>Suporte</a>
+    <a id='revoltadin' className='link' href='#'>Sobre Nós</a>
+    <a id='revoltadin2' className='link' href='#'>Orçamento</a>
+    <a id='revoltadin3' className='link' href='#'>Contato</a>
+    <a id='revoltadin4' className='link' href='#'>Suporte</a>
     <img className='img' src='https://cdn.discordapp.com/attachments/1255626890610213066/1256416740196483083/LOGO.png?ex=6713b005&is=67125e85&hm=6d849073462eebc4e7f4dcfd77c5f32193a3bc865b4940f59fa6e76d6d7f239f&' alt='' />
     </div>
 
     const Div2 = () =>   <div className='a'>
     <img className='foto' src='https://www.next4.com.br/wp-content/themes/yootheme/cache/25/desenvolvimento-de-sites-2591cf1d.webp' />
-    <h1 className='text'>Planejamento e <br></br> Desenvolvimento de <br></br> Websites</h1>
+    <h1 id='text' className='text'>Planejamento e <br></br> Desenvolvimento de <br></br> Websites</h1>
     </div>
 
     const Div3 = () => <div className='b'>
@@ -40,7 +40,7 @@ export default function home(){
     
   
     const Div5 = () => <div className='faixa-verde'> 
-        <h1>Venha fazer seu <br></br>orçamento!</h1>
+        <h1 id='kakaka'>Venha fazer seu <br></br>orçamento!</h1>
 
         <button id="botaoo" className="contact-button">
                 Orçamento para desenvolvimento de sites 
@@ -97,14 +97,12 @@ export default function home(){
            <a className='linkk' id='link4' href='#'>Informações</a>
         </div>
 
-        const Div11 = () => <div className='logo'>
-        
-        </div>
+   
 
-   const Div12 = () => {
+  
        
     
-};
+
 
     return(
     <>
@@ -118,8 +116,8 @@ export default function home(){
          <Div8 />
          <Div9 />
          <Div10/>
-         <Div11/>
-         <Div12/>
+        
+        
      </> 
          
 )       
